7b422742ed6b30d102571865ae880bd0d79f8040
sunls24 <sunls24@outlook.com>
2023-11-03 13:15:51 +0800

    Add auto refresh

--- components/mail-refresh.tsx
+++ components/mail-refresh.tsx
@@ -1,16 +1,37 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { RefreshCw } from "lucide-react";
+import { refreshSeconds } from "@/lib/constant";
 
 function MailRefresh() {
+  const [seconds, setSeconds] = useState(0);
+  useEffect(() => {
+    const id = setInterval(() => {
+      if (seconds >= refreshSeconds - 1) {
+        onRefresh();
+        return;
+      }
+      setSeconds(seconds + 1);
+    }, 1000);
+    return () => clearInterval(id);
+  }, [seconds]);
+
+  function onRefresh() {
+    console.log("refresh");
+    setSeconds(0);
+  }
+
   return (
     <>
-      <Button variant="outline">
+      <Button variant="outline" onClick={onRefresh}>
         <RefreshCw size={16} className="mr-1" />
         刷新
       </Button>
       <span className="self-end pb-0.5 text-sm text-muted-foreground">
-        将在3s后自动刷新
+        将在
+        <span className="font-medium">{refreshSeconds - seconds}s</span>
+        后自动刷新
       </span>
     </>
   );
--- lib/constant.ts
+++ lib/constant.ts
@@ -1,3 +1,7 @@
 export const domainList = (process.env.NEXT_PUBLIC_DOMAIN_LIST ?? "")
   .split("|")
   .map((v) => "@" + v);
+
+export const refreshSeconds = parseInt(
+  process.env.NEXT_PUBLIC_REFRESH_SECONDS ?? "10",
+);
