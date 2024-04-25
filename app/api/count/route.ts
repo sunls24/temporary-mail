322878376c654f505f47b93db0e8ac67857b1e82
sunls24 <sunls24@outlook.com>
2024-04-25 17:46:41 +0800

    Fix github action build

--- app/api/count/route.ts
+++ app/api/count/route.ts
@@ -2,6 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { fetchLast10Day } from "@/app/api/fetch";
 
 export async function GET(req: NextRequest) {
+  // No need to execute fetch* when packaging (don't know why, but it works)
+  req.headers.get("host");
   try {
     const data = await fetchLast10Day();
     return NextResponse.json(data);
